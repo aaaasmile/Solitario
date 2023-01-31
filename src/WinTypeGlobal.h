70c1288c975552d964a4a89445d2775e8135c28f
Igor Sarzi Sartori <i.sartori@navax.com>
2023-01-31 13:31:33 +0100

    rework trace

--- src/WinTypeGlobal.h
+++ src/WinTypeGlobal.h
@@ -2,22 +2,12 @@
 #define _WINTYPEGLOBAL__H_
 
 #if _MSC_VER > 1000
-#include <StdAfx.h>
-#endif
-
-#ifdef USEDIALOGTRACE
-#include <iostream>
-#endif
-
-#if _MSC_VER > 1000
 #pragma warning(disable : 4786)
 #pragma warning(disable : 4996)
 #include <windows.h>
 #endif
 
 #include <stdarg.h>
-
-#include <deque>
 #include <string>
 #include <vector>
 
@@ -34,38 +24,45 @@ typedef const char *LPCSTR, *PCSTR;
 #endif
 
 #ifndef TRACE
-#include <stdio.h>
-extern void TraceInLogFile(char* myBuff);
-#ifdef TRACEINDEBUGGER
-inline void TRACE(const char* fmt, ...) {
-    char myBuff[512];
-    va_list args;
+    #ifdef TRACEINDEBUGGER
+    inline void TRACE(const char* fmt, ...) {
+        char myBuff[512];
+        va_list args;
+        va_start(args, fmt);
+        int result = vsprintf(myBuff, fmt, args);
+        ::OutputDebugString(myBuff);
+    }
+    #else
+        #ifdef TRACEINSERVICE
+            #include <stdio.h>
+            extern void TraceInLogFile(char* myBuff);
 
-    va_start(args, fmt); /* Initialize variable arguments. */
-
-    int result = vsprintf(myBuff, fmt, args);
-#ifdef USEDIALOGTRACE
-    std::cout << "[TR] " << myBuff;
-#else
-    ::OutputDebugString(myBuff);
-    TraceInLogFile(myBuff);
-#endif
-}
-#else
-#ifdef USETRACE
-inline void TRACE(const char* fmt, ...) {
-    char myBuff[1024];
-    va_list args;
-    va_start(args, fmt);
-    vsprintf(myBuff, fmt, args);
-    printf("[TR]%s\n", myBuff);
-}
-#else
-inline void TRACE(const char* fmt, ...) {}
-#endif
-#endif
+            inline void TRACE(const char* fmt, ...) {
+                char myBuff[1024];
+                va_list args;
+                va_start(args, fmt);
+                vsprintf(myBuff, fmt, args);
+                TraceInLogFile(myBuff);
+            }
+        #else
+            #ifdef TRACEINSTD
+                inline void TRACE(const char* fmt, ...) {
+                    char myBuff[1024];
+                    va_list args;
+                    va_start(args, fmt);
+                    vsprintf(myBuff, fmt, args);
+                    sprintf(stdout, myBuff);
+                }
+            #else   
+                inline void TRACE(const char* fmt, ...) {}
+            #endif
+        #endif
+    #endif
 #endif
 
+#ifndef VCT_STRING
 typedef std::vector<std::string> VCT_STRING;
+#endif 
+
 
 #endif
